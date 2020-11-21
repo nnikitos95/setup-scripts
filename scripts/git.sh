d7ec280cfe4331570e4d900847cfadfa0886f883
Nikita Popov <nnikitos95@mail.ru>
2020-11-21 20:52:45 +0300

    Separate git install and git aliases

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,15 +3,3 @@ echo 'Install git'
 sudo apt-get update
 sudo apt-get install git
 echo 'Done'
-
-#set aliases
-echo 'Set aliases'
-git config --global alias.ct checkout
-git config --global alias.br branch
-git config --global alias.cmt commit
-git config --global alias.st status
-git config --global alias.pl pull
-git config --global alias.ph push
-git config --global alias.ctb 'checkout -b'
-git config --global alias.mg merge
-echo 'Done'

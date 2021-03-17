9bbb1db7b41e9471c0641ffbc62e8bb0eb3ac7ff
Nikita Popov <nnikitos95@mail.ru>
2021-03-17 14:57:39 +0300

    Update git_aliases.sh
    Добавлена команда, выводящая ветки с учетом последних коммитов

--- scripts/git_aliases.sh
+++ scripts/git_aliases.sh
@@ -8,4 +8,5 @@ git config --global alias.pl pull
 git config --global alias.ph push
 git config --global alias.ctb 'checkout -b'
 git config --global alias.mg merge
+git config --global alias.brlf "branch --sort=-committerdate --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(contents:subject)%(color:reset) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 echo 'Done'
